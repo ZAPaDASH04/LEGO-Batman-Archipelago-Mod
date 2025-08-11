ad3b65c1cf336d83dee06df4f1fe3c073b0d2e65
ZAPaDASH04 <ZAPaDASH04@gmail.com>
2025-08-11 17:29:35 -0400

    fixed compiler and replaced ap.dll's

--- export.bat
+++ export.bat
@@ -2,7 +2,7 @@ REM Run compile.bat and wait for it to finish
 call .\compile.bat
 
 REM Copy files from injector\bin to export
-xcopy /Y /E /I .\injector\bin\* .\export\
+rem xcopy /Y /E /I .\injector\bin\* .\export\
 
 REM Copy files from hack\bin to export
 xcopy /Y /E /I .\hack\bin\* .\export\
