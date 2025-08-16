441e7a794591ca41ddde9934a7ccfbc002f8e801
ZAPaDASH04 <ZAPaDASH04@gmail.com>
2025-08-16 13:02:10 -0400

    updated export.bat

--- export.bat
+++ export.bat
@@ -1,5 +1,6 @@
 rem Run compile.bat and wait for it to finish
 call .\compile.bat
 xcopy /Y /E /I .\Reloadedii\* .\export\LB1AP
+cp .\README.md .\export\LB1AP\README.md
 
 powershell Compress-Archive -Path ".\export\LB1AP\*" -DestinationPath ".\export\LB1APv0.1.1a.zip" -Force
