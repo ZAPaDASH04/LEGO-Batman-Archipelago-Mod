e91140fd528e26340ce25f8fa4b1a557c6e591cd
ZAPaDASH04 <ZAPaDASH04@gmail.com>
2025-08-15 22:12:10 -0400

    cleaned up a lot of stuff. renamed mod folder

--- compile.bat
+++ compile.bat
@@ -1,16 +1,2 @@
 echo off
-
-rem start "" cmd /c "i686-w64-mingw32-g++ -m32 -o \"C:\Program Files (x86)\Steam\steamapps\common\Lego Batman\injector.exe\" .\injector\src\injector.cpp 1>injector_log.txt 2>&1"
-rem start "" cmd /c "i686-w64-mingw32-g++ -shared -m32 -o \"C:\Program Files (x86)\Steam\steamapps\common\Lego Batman\mod.dll\" .\hack\src\mod.cpp -static-libgcc -static-libstdc++ 1>mod_log.txt 2>&1"
-
-rem set "GAMEPATH=C:\Progra~2\Steam\steamapps\common\Lego Batman"
-rem start "" cmd /c "i686-w64-mingw32-g++ -m32 -o \"%GAMEPATH%\injector.exe\" .\injector\src\injector.cpp 1>injector_log.txt 2>&1"
-rem start "" cmd /c "i686-w64-mingw32-g++ -shared -m32 -o \"%GAMEPATH%\mod.dll\" .\hack\src\mod.cpp -static-libgcc -static-libstdc++ >mod_log.txt 2>&1"
-
-
-rem i686-w64-mingw32-g++ -m32 -o "C:\Program Files (x86)\Steam\steamapps\common\Lego Batman\LB1I.exe" .\injector\src\injector.cpp -static-libgcc -static-libstdc++ -static 1>injector_log.txt 2>&1
-rem make -C .\injector && cp .\injector\bin\LB1I.exe  "C:\Program Files (x86)\Steam\steamapps\common\Lego Batman\LB1I.exe"
-
-rem i686-w64-mingw32-g++ -shared -m32 -o "C:\Program Files (x86)\Steam\steamapps\common\Lego Batman\ap.dll" .\hack\src\mod.cpp -static-libgcc -static-libstdc++ -static 1>mod_log.txt 2>&1
-rem make -C .\hack && cp .\hack\bin\ap.dll "C:\Program Files (x86)\Steam\steamapps\common\Lego Batman\ap.dll"
-make -C .\hack && cp .\hack\bin\ap.dll .\Reloadedii\legobatman1.apsupport
+make -C .\mod && cp .\mod\bin\ap.dll .\Reloadedii\legobatman1.apsupport
--- export.bat
+++ export.bat
@@ -1,10 +1,4 @@
 rem Run compile.bat and wait for it to finish
 call .\compile.bat
 
-rem Copy files from injector\bin to export
-rem xcopy /Y /E /I .\injector\bin\* .\export\
-
-rem Copy files from hack\bin to export
-rem xcopy /Y /E /I .\hack\bin\* .\export\
-rem cp .\hack\bin\ap.dll
 xcopy /Y /E /I .\Reloadedii\* .\export\LB1AP
