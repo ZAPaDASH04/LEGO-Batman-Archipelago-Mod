5cdda741fe99eaa799f760f251b68629581d8d1e
ZAPaDASH04 <ZAPaDASH04@gmail.com>
2025-08-15 22:37:25 -0400

    now it's fixed

--- export.bat
+++ export.bat
@@ -1,4 +1,5 @@
 rem Run compile.bat and wait for it to finish
 call .\compile.bat
+xcopy /Y /E /I .\Reloadedii\* .\export\LB1AP
 
-xcopy /Y /E /I .\Reloadedii\* .\export\LB1AP & tar -a -c -f ".\export\LB1APv0.1.1a.zip" ".\export\LB1AP"
+powershell Compress-Archive -Path ".\export\LB1AP\*" -DestinationPath ".\export\LB1APv0.1.1a.zip" -Force
