8130bfe750d79b9bec033f86cde3b22cec85eaf7
ZAPaDASH04 <ZAPaDASH04@gmail.com>
2025-09-01 20:28:40 -0400

    fixed powerbrick infinite loop

--- mod/src/mod.cpp
+++ mod/src/mod.cpp
@@ -505,7 +505,7 @@ DWORD WINAPI ThreadProc(LPVOID lpParam) {
                     << std::endl;
                 // WARN: high probability to fail. NEEDS SIGNIFICANT TESTING    
                 LB1AP_send_item(LB1AP_LOCATION_ID_OFFSET + 485 + sublevelToLevel(game.currentLevel));
-                game.inLevelPowerBrickPrev = 1;
+                game.inLevelPowerBrickPrev = 2;
             }
 
         }
